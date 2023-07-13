a93cd87da40840cb26bdecf2ebde554bd16e69e1
yguvvala <quic_yguvvala@quicinc.com>
2023-07-13 15:04:04 +0530

    iot-core-oss: Add the c2-module code and the conditions for LE specific code
    Change-Id: Iad0123d07f726dfe11a474e047d1348bd982f102

---
+++ c2-module/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE = libqtic2module
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_CPP_EXTENSION := .cc
+LOCAL_CPPFLAGS += -fexceptions
+
+LOCAL_SRC_FILES := \
+    c2-module.cc
+
+LOCAL_SHARED_LIBRARIES := \
+    libcodec2 \
+    libqcodec2_core \
+    libcodec2_vndk
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libqtic2headers
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
+include $(BUILD_HEADER_LIBRARY)
