08916827a61c4fcab01cc64aae21ddfce6116960
Jean Xiao <quic_jianxiao@quicinc.com>
2024-04-10 17:18:04 +0800

    iot-core-oss: select video encoder buffer pool
    Enable encoder output buffer pool with linear non-contiguous type.
    
    Change-Id: I4e87b8d2f252b8032a79a8ea43813c5b508610a6

--- c2-module/c2-module.cc
+++ c2-module/c2-module.cc
@@ -158,14 +158,17 @@ c2_status_t C2Module::Initialize(std::shared_ptr<IC2Notifier>& notifier) {
   state_ = State::kIdle;
 
 #if defined(CODEC2_CONFIG_VERSION_2_0)
-  if (mode_ != C2ModeType::kVideoDecode) {
-    // Output buffer pool for the encoder and audio is not properly supported.
+  if (mode_ != C2ModeType::kVideoDecode && mode_ != C2ModeType::kVideoEncode) {
+    // Output buffer pool for audio is not properly supported.
     return C2_OK;
   }
 
   // Create output graphic/linear pool for buffer circulation.
   ::android::C2PlatformAllocatorStore::id_t type =
       C2AllocatorStore::GRAPHIC_NON_CONTIGUOUS;
+
+  if (mode_ == C2ModeType::kVideoEncode)
+    type = C2AllocatorStore::LINEAR_NON_CONTIGUOUS;
 
   std::shared_ptr<C2BlockPool> pool;
   status = ::android::CreateCodec2BlockPool(type, component_, &pool);
@@ -176,9 +179,13 @@ c2_status_t C2Module::Initialize(std::shared_ptr<IC2Notifier>& notifier) {
   }
 
   C2BlockPool::local_id_t id;
-
-  graphic_mem_ = std::make_shared<C2GraphicMemory>(pool);
-  id = graphic_mem_->GetLocalId();
+  if (mode_ == C2ModeType::kVideoDecode) {
+    graphic_mem_ = std::make_shared<C2GraphicMemory>(pool);
+    id = graphic_mem_->GetLocalId();
+  } else {
+    linear_mem_ = std::make_shared<C2LinearMemory>(pool);
+    id = linear_mem_->GetLocalId();
+  }
 
   // Register the buffer pool ID so that it is used by the component.
   auto pools = C2PortBlockPoolsTuning::output::AllocUnique({id});
